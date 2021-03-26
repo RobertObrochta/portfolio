7c4bb82715285b00ea6be53eee4fab8ddbd42d98
Robert Obrochta <obrochtarobert13@gmail.com>
2021-03-26 12:16:32 -0500

    Updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,54 @@
 
 import React from 'react';
-import Python from './img/python.png'
-import ReactJS from './img/ReactJS.png'
 
 const About = () =>  {
   return (
     <div className="pageSpace about-section" id="About">
       <div className="about-words">
-        <h3>I am a current third year undergraduate student at DePaul University in Chicago. 
+        <p>I'm a current third year undergraduate student at DePaul University in Chicago. 
           I come from 2.5 years as a music education major, and recently moved fully to computer science. 
-          In the world of music, I am fascinated with how different musical cultures interact with one another.</h3>
+          In the world of music, I'm fascinated with how different musical cultures interact with one another.</p>
           <br></br>
-          <h3>In the world of computer science, I am fascinated with the same concept of how people interact with each other,
+          <p>In the world of computer science, I'm fascinated with the same concept of how people interact with each other,
             but now in the world of technology. I see computer science as a tool that can help people immensely in many different aspects. 
             During my computer science career, I have the goal of discovering the different ways that 
             I can more effectively impact a broad audience. Computer science draws me in because I love the learning process, 
-            and there are just so many things to achieve in this field.</h3>
+            and there are just so many things to achieve in this field.</p>
             <br></br>
-            <h3>For my hobbies, I keep up with practicing trumpet and piano. I go on 10+ mile bike rides almost every day 
-              during the warmer months and am an avid nature-lover.</h3>
+            <p>For my hobbies, I keep up with practicing trumpet and piano. I go on 10+ mile bike rides almost every day 
+              during the warmer months and am an avid nature-lover.</p>
           </div>
             
-          {/* list all tech stack and pictures for each */}
-          <div className="about-tech">
-            <h2 className="tech-header">Technologies that I'm comfortable with</h2>
-              <h3>Back End: Python, SQL, Java</h3><img src={Python} alt='Python' className='techstack'></img>
-              <h3>Front End: React.js, React Native, and HTML/CSS/JS</h3><img src={ReactJS} alt='React.js' className='techstack'></img>
+          {/* list all tech stack */}
+            <div className="about-tech">
+              <h2 className="tech-header">Technologies that I'm comfortable with</h2>
+                <div className="inline tech-space">
+                  <div className="tech-space">
+                    <h3><b>Back End</b></h3> 
+                      <ul className="within-space">
+                        <li>Python</li>
+                        <li>SQL</li>
+                        <li>Java</li>
+                      </ul>
+                  </div>
+                  <div className="tech-space">
+                    <h3><b>Front End</b></h3>
+                      <ul className="within-space">
+                        <li>React.js</li>
+                        <li>HTML/CSS/JS</li>
+                        <li>React Native</li>
+                      </ul>
+                  </div>
+                  <div className="tech-space">
+                    <h3><b>Other</b></h3>
+                      <ul className="within-space">
+                        <li>Git</li>
+                        <li>Linux</li>
+                      </ul>
+                  </div>
+                </div>
+            </div>
           </div>
-    </div>
   );
   
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ import linkedinLogo from './img/linkedinlogo.png'
 const Footer = () => {
     return (
         <footer>
-                <a href="https://github.com/RobertObrochta" target="blank"><img src={githubLogo} alt="GitHub" className="github"></img></a>
-                <a href="https://www.linkedin.com/in/robert-obrochta/" target="blank"><img src={linkedinLogo} alt="LinkedIn" className="linkedin"></img></a>
+                <a href="https://github.com/RobertObrochta" target="_blank" rel="noreferrer nofollow"><img src={githubLogo} alt="GitHub" className="github"></img></a>
+                <a href="https://www.linkedin.com/in/robert-obrochta/" target="_blank" rel="noreferrer nofollow"><img src={linkedinLogo} alt="LinkedIn" className="linkedin"></img></a>
         </footer>
     )
 }
